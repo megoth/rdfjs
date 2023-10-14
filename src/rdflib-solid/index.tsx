ecff923637de54798af52f4939671a36c929bdc3
Arne Hassel <arne.hassel@capgemini.com>
2023-10-14 05:20:41 +0200

    Improving auth in rdflib-solid

--- src/rdflib-solid/index.tsx
+++ src/rdflib-solid/index.tsx
@@ -1,18 +1,21 @@
 import RdflibSolidDemo from "./demo";
 import Login from "../login";
-import {EVENTS, getDefaultSession} from '@inrupt/solid-client-authn-browser'
-import {useState} from "react";
+import {getDefaultSession, handleIncomingRedirect} from '@inrupt/solid-client-authn-browser'
+import {useEffect, useState} from "react";
 
 export default function RdflibSolid() {
     const session = getDefaultSession();
     const [isLoggedIn, setIsLoggedIn] = useState(session.info.isLoggedIn);
-    session.on(EVENTS.LOGIN, () => setIsLoggedIn(session.info.isLoggedIn));
-    session.on(EVENTS.SESSION_RESTORED, () => setIsLoggedIn(session.info.isLoggedIn));
-    session.on(EVENTS.SESSION_EXPIRED, () => setIsLoggedIn(session.info.isLoggedIn));
+
+    useEffect(() => {
+        handleIncomingRedirect({
+            restorePreviousSession: true
+        }).then((info) => setIsLoggedIn(!!info?.isLoggedIn));
+    }, []);
 
     const login = async (providerUrl: string) => session.login({
         oidcIssuer: providerUrl,
-        clientName: "RDF + JS = &#10084; (demo app)",
+        clientName: "RDF + JS = HEART (demo app)",
     });
 
     const logout = async () => {
