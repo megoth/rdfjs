7f6a4d682baaa1cc00988dcd58d6dd75159cb6ac
Arne Hassel <arne.hassel@capgemini.com>
2023-10-14 05:08:26 +0200

    Extracted some functionality into its own component

--- src/frontpage/index.tsx
+++ src/frontpage/index.tsx
@@ -1,34 +1,14 @@
 import Intro from "./intro.mdx";
 import SolidIntro from "./solidIntro.mdx";
 import Content from "../content";
-import Login from "../login";
-import {useSolidAuth} from "@ldo/solid-react";
+import SolidWarning from "../solid-warning";
 
 export default function Frontpage() {
-    const {session, login, logout} = useSolidAuth();
     return (
         <Content>
             <Intro/>
             <SolidIntro/>
-            {session.isLoggedIn ? <>
-                <div className="message is-success">
-                    <div className="message-body">
-                        <span>You're logged in successfully!</span>
-                    </div>
-                </div>
-                <button className="button is-small" onClick={logout}>Log out</button>
-            </> : <>
-                <div className="message is-warning">
-                    <div className="message-body">
-                        For the demo we will need at least <strong>READ</strong> and <strong>APPEND</strong> access to
-                        your WebID resource. Even this provides an app with a lot of possibilities for malicious intent.
-                        So to be sure you should test this app with WebID that isn't vulnerable (e.g. <u>use a test
-                        account</u>).
-                    </div>
-                </div>
-                <p>If you want, you can log in here, to verify that everything connects as it should.</p>
-                <Login login={login} />
-            </>}
+            <SolidWarning/>
         </Content>
     );
 }
---
+++ src/solid-warning/index.tsx
@@ -0,0 +1,26 @@
+import {useSolidAuth} from "@ldo/solid-react";
+import Login from "../login";
+
+export default function SolidWarning() {
+    const {session, login, logout} = useSolidAuth();
+
+    return session.isLoggedIn ? <>
+        <div className="message is-success">
+            <div className="message-body">
+                <span>You're logged in successfully!</span>
+            </div>
+        </div>
+        <button className="button is-small" onClick={logout}>Log out</button>
+    </> : <>
+        <div className="message is-warning">
+            <div className="message-body">
+                For the demo we will need at least <strong>READ</strong> and <strong>APPEND</strong> access to
+                your WebID resource. Even this provides an app with a lot of possibilities for malicious intent.
+                So to be sure you should test this app with WebID that isn't vulnerable (e.g. <u>use a test
+                account</u>).
+            </div>
+        </div>
+        <p>If you want, you can log in here, to verify that everything connects as it should.</p>
+        <Login login={login}/>
+    </>;
+}
