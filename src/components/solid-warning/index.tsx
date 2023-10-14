c128c67e641f690a6cc553407908b5cd9d6c343b
Arne Hassel <arne.hassel@capgemini.com>
2023-10-14 23:06:15 +0200

    Added name to the Solid warning

--- src/components/solid-warning/index.tsx
+++ src/components/solid-warning/index.tsx
@@ -1,30 +1,40 @@
-import {useSolidAuth} from "@ldo/solid-react";
+import {useResource, useSolidAuth, useSubject} from "@ldo/solid-react";
 import Login from "../login";
+import {SolidProfileShapeType} from "ldo-solid-profile";
+import Loading from "../loading";
 
 export default function SolidWarning() {
     const {session, login, logout} = useSolidAuth();
+    useResource(session.webId);
+    const profile = useSubject(SolidProfileShapeType, session.webId);
 
     return <>
         <h3 className={"subtitle is-4"}>A note on the use of Solid</h3>
-        {session.isLoggedIn ? <>
-            <div className="message is-success">
-                <div className="message-body">You're all good! (Thanks for trusting us, we won't abuse that trust.)
+        {session.isLoggedIn ?
+            profile?.name ? <>
+                <div className="message is-success">
+                    <div className="message-body">
+                        Thanks, {profile?.name || "Stranger"}, you're all good!
+                        (Thanks for trusting us, we won't abuse that trust.)
+                    </div>
                 </div>
-            </div>
-            <button className="button is-small" onClick={logout}>Log out</button>
-        </> : <>
-            <div className="message is-warning">
-                <div className="message-body">
-                    The demos allow you to change the name described in your WebID profile. This means we will need at
-                    least <strong>READ</strong> and <strong>APPEND</strong> access to your WebID resource. The latter
-                    access mode provides a lot of possibilities for an app with malicious intent. Although we can
-                    promise we won't betray that trust (and we do, pinky promise!), but you might want to consider
-                    using this app (and other experimental apps like it) with a WebID that isn't vulnerable
-                    (e.g. use a test account).
+                <button className="button is-small" onClick={logout}>Log out</button>
+            </> : <Loading/>
+            : <>
+                <div className="message is-warning">
+                    <div className="message-body">
+                        The demos allow you to change the name described in your WebID profile. This means we will need
+                        at
+                        least <strong>READ</strong> and <strong>APPEND</strong> access to your WebID resource. The
+                        latter
+                        access mode provides a lot of possibilities for an app with malicious intent. Although we can
+                        promise we won't betray that trust (and we do, pinky promise!), but you might want to consider
+                        using this app (and other experimental apps like it) with a WebID that isn't vulnerable
+                        (e.g. use a test account).
+                    </div>
                 </div>
-            </div>
-            <p>If you want, you can log in here, to verify that everything connects as it should.</p>
-            <Login login={login}/>
-        </>}
+                <p>If you want, you can log in here, to verify that everything connects as it should.</p>
+                <Login login={login}/>
+            </>}
     </>;
 }
