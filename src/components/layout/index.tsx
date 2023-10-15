08369d505b4a7d9362e80c9d53f5c233e87a1f6a
Arne Hassel <arne.hassel@capgemini.com>
2023-10-15 08:39:50 +0200

    Fixed a couple of smaller bugs

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,15 @@
 import Navigation from "../navigation";
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import styles from "./style.module.css";
 import Footer from "./footer";
+import {useLayoutEffect} from "react";
 
 export default function Layout() {
+    const location = useLocation();
+    useLayoutEffect(() => {
+        document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+
     return (
         <div className="container">
             <Navigation/>
