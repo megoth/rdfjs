7dea69b703d054d4de7637189146df0b9d5e3436
Arne Hassel <arne.hassel@capgemini.com>
2023-10-15 20:38:31 +0200

    Fixed a small React bug in ratings

--- src/components/rating/index.tsx
+++ src/components/rating/index.tsx
@@ -1,5 +1,6 @@
 import {BsFillHeartFill, BsHeart} from "react-icons/bs";
 import styles from "./style.module.css";
+import {Fragment} from "react";
 
 interface Props {
     rating: 1 | 2 | 3 | 4 | 5;
@@ -8,9 +9,11 @@ interface Props {
 export default function Rating({rating}: Props) {
     const stars = Array.from(Array(5)).map((_, index) => index < rating)
     return <span className={styles.rating}>
-        {stars.map((isRated) => isRated ?
-            <BsFillHeartFill className={styles.icon}/> :
-            <BsHeart className={styles.icon}/>
-        )}
+        {stars.map((isRated, index) => <Fragment key={`rating-${index}`}>
+            {isRated ?
+                <BsFillHeartFill className={styles.icon}/> :
+                <BsHeart className={styles.icon}/>
+            }
+        </Fragment>)}
     </span>;
 }
