eeedab82c3085d49705c77c921d5e2801a0b3d87
Arne Hassel <arne.hassel@capgemini.com>
2023-10-15 02:01:26 +0200

    Improved dev mode button a bit

--- src/components/developer-mode-button/index.tsx
+++ src/components/developer-mode-button/index.tsx
@@ -1,12 +1,14 @@
 import {useContext} from "react";
 import DeveloperModeContext from "../../contexts/developer-mode";
+import styles from "./style.module.css";
 
 export default function DeveloperModeButton() {
     const {developerMode, setDeveloperMode} = useContext(DeveloperModeContext);
 
     return (
         <button className="button is-dark is-small" onClick={() => setDeveloperMode(!developerMode)}>
-            Developer mode is {developerMode ? "on" : "off"}
+            <span className={styles.largeText}>Developer mode is {developerMode ? "on" : "off"}</span>
+            <span className={styles.smallText}>dev {developerMode ? "on" : "off"}</span>
         </button>
     );
 }
