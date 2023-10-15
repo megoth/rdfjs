2c32625f647d00a1d9767066ac5ccdafaef158b6
Arne Hassel <arne.hassel@capgemini.com>
2023-10-15 04:59:28 +0200

    Added React Icons instead of html codes

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,6 +2,7 @@ import {NavLink} from "react-router-dom";
 import {clsx} from "clsx";
 import {Fragment, useState} from "react";
 import styles from "./style.module.css";
+import {BiHomeHeart} from "react-icons/bi";
 
 interface Link {
     href: string;
@@ -64,7 +65,10 @@ export default function Navigation() {
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="container">
                 <div className="navbar-brand">
-                    <NavLink to={"/"} className="navbar-item">⛫ Home</NavLink>
+                    <NavLink to={"/"} className={clsx("navbar-item", styles.homeLink)}>
+                        <BiHomeHeart />
+                        <span>Home</span>
+                    </NavLink>
                     <button className={clsx("navbar-burger", {
                         "is-active": isActive
                     })} aria-label="menu" aria-expanded="false" type="button"
