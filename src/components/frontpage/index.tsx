0217514c84fce91be7c75eaeb82834b5bc1a7322
Arne Hassel <arne.hassel@capgemini.com>
2023-10-17 06:48:42 +0200

    Split off local section into its own part from intro

--- src/components/frontpage/index.tsx
+++ src/components/frontpage/index.tsx
@@ -1,23 +1,25 @@
 import Intro from "./1-intro.mdx";
-import Solid from "./2-solid.mdx";
-import Reviews from "./3.0-reviews.mdx";
+import Local from "./2-local.mdx";
+import Solid from "./3-solid.mdx";
+import Reviews from "./4.0-reviews.mdx";
 import SolidWarning from "../solid-warning";
 import DemoList from "../demo-list";
 import {LOCAL_DEMOS, SOLID_DEMOS} from "../../constants.ts";
-import Demo from "./4-demo.mdx";
-import About from "./5-about.mdx";
+import Demo from "./5-demo.mdx";
+import About from "./6-about.mdx";
 
 export default function Frontpage() {
     return (
         <>
             <Intro/>
-            <DemoList list={LOCAL_DEMOS} />
+            <Local/>
+            <DemoList list={LOCAL_DEMOS}/>
             <Solid/>
             <SolidWarning/>
-            <DemoList list={SOLID_DEMOS} />
+            <DemoList list={SOLID_DEMOS}/>
             <Reviews/>
-            <Demo />
-            <About />
+            <Demo/>
+            <About/>
         </>
     );
 }
