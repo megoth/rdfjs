97461240af3bc880bfd3e361cb3af63b497a68f0
Arne Hassel <arne.hassel@capgemini.com>
2023-10-20 00:13:18 +0200

    Slight mistake before

--- src/components/library-header/index.tsx
+++ src/components/library-header/index.tsx
@@ -13,7 +13,7 @@ export default function LibraryHeader({library}: Props) {
                 <img src={library.icon} alt={library.iconAlt} style={{maxHeight: library.iconPreferredHeight}}/>
             </picture>
             <header className="content">
-                <h1 className="title is-4">{library.name}</h1>
+                <h1 className="title">{library.name}</h1>
                 <ul className={styles.libraryInfo}>
                     <li>Project page: <a href={library.websiteUrl}>{library.websiteName}</a></li>
                     <li>Created by <a href={library.creatorUrl}>{library.creator}</a></li>
