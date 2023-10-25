746d27cd248f7a54445fbce77697d1bd2ae098a6
Arne Hassel <arne.hassel@capgemini.com>
2023-10-25 03:42:19 +0200

    Tightening up text some more

--- src/components/solid-guide/authentication-demo.tsx
+++ src/components/solid-guide/authentication-demo.tsx
@@ -1,19 +1,20 @@
 import {useSolidAuth} from "@ldo/solid-react";
-import Content from "../content";
 import Login from "../login";
 
 export default function AuthenticationDemo() {
     const {login, logout, session} = useSolidAuth();
 
-    return <Content>
-        {session.isLoggedIn
-            ? <>
-                <p>You're authenticated with <a href={session.webId}>{session.webId}</a></p>
-                <button className="button is-small" onClick={logout}>Log out</button>
-            </>
-            : <>
-                <p>You're not authenticated</p>
-                <Login login={login}/>
-            </>}
-    </Content>
+    return session.isLoggedIn
+        ? <>
+            <div className="notification is-success is-light">
+                You're authenticated with <a href={session.webId}>{session.webId}</a>
+            </div>
+            <button className="button" onClick={logout}>Log out</button>
+        </>
+        : <>
+            <div className="notification is-danger is-light">
+                You're not authenticated
+            </div>
+            <Login login={login}/>
+        </>
 }
