015fab54bafdb7ab0ecd538baec4195bb7439cdf
Arne Hassel <arne.hassel@capgemini.com>
2023-10-25 03:54:03 +0200

    Fixing up typography a bit more

--- src/components/guide-header/index.tsx
+++ src/components/guide-header/index.tsx
@@ -8,13 +8,8 @@ interface Props {
 
 export default function GuideHeader({guide}: Props) {
     return (
-        <>
-            <picture className={clsx("image", styles.image)}>
-                <img src={guide.logo} alt={guide.logoAlt} style={{maxHeight: guide.logoPreferredHeight}}/>
-            </picture>
-            <header className="content">
-                <h1 className="title">{guide.name}</h1>
-            </header>
-        </>
+        <picture className={clsx("image", styles.image)}>
+            <img src={guide.logo} alt={guide.logoAlt} style={{maxHeight: guide.logoPreferredHeight}}/>
+        </picture>
     );
 }
