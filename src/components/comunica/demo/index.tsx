2e838950046cfce2560a64c7fd638c7bf980581c
Arne Hassel <arne.hassel@capgemini.com>
2023-10-26 13:13:03 +0200

    Added first draft for Comunica

--- src/components/comunica/demo/index.tsx
+++ src/components/comunica/demo/index.tsx
@@ -1,34 +1,34 @@
 import Demo, {FormData} from "../../demo";
 import {QueryEngine} from "@comunica/query-sparql";
-import {useEffect, useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import {useSolidAuth} from "@ldo/solid-react";
+
+const engine = new QueryEngine();
 
 export default function ComunicaDemo() {
     const {fetch, session} = useSolidAuth();
-    const engine = useMemo(() => new QueryEngine(), []);
     const [name, setName] = useState("");
 
     useEffect(() => {
-        if (!session.isLoggedIn || !session.webId) return;
+        if (!session.webId) return;
 
         engine.queryBindings(`
         PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
         SELECT ?name WHERE {
             <${session.webId}> foaf:name ?name
-        } LIMIT 1`
-            , {
-                fetch,
-                sources: [session.webId],
-            }).then(async (stream) => {
+        } LIMIT 1`, {
+            fetch,
+            sources: [session.webId],
+        }).then(async (stream) => {
             const bindings = await stream.toArray();
             const foundName = bindings[0].get("name")?.value || "Stranger";
             setName(foundName);
         })
-    }, [engine, fetch, session.isLoggedIn, session.webId]);
+    }, [fetch, session.webId]);
 
 
     const onSubmit = async (data: FormData) => {
-        if (!session.isLoggedIn || !session.webId) return;
+        if (!session.webId) return;
 
         await engine.queryVoid(`
         PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
