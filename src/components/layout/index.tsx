0b7540380b80fcdf67c3f50c13598712fced138c
Arne Hassel <arne.hassel@capgemini.com>
2023-10-26 03:09:14 +0200

    Improving message on feedback

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,12 +8,12 @@ export default function Layout() {
         <div className="container">
             <Navigation/>
             <main className={styles.main}>
-                <div className="message is-light is-small">
-                    <div className="message-header">Website under review</div>
+                <div className="message is-info is-small">
                     <div className="message-body">
-                        This website is currently being review before final launch. If you have any notes that you want
-                        to share with me, please DM me if you can or create an issue on the <a
-                        href="https://github.com/megoth/rdfjs/issues">website's project page</a>.
+                        This website is currently being review before final launch. If you have any feedback on this
+                        website that you want to share with me, I would appreciate it if you send me a message (on a
+                        channel you know me) or create an issue on the&nbsp;
+                        <a href="https://github.com/megoth/rdfjs/issues">website's project page</a>.
                     </div>
                 </div>
                 <Outlet/>
