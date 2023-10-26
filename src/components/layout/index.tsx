422c99c2cf669a8ba471520b042d3affdd26c391
Arne Hassel <arne.hassel@capgemini.com>
2023-10-26 02:38:41 +0200

    Updated message on top to notify that website is now under review
    I want to invite others to review my work before spreading it

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -9,9 +9,11 @@ export default function Layout() {
             <Navigation/>
             <main className={styles.main}>
                 <div className="message is-light is-small">
-                    <div className="message-header">Work in progress</div>
+                    <div className="message-header">Website under review</div>
                     <div className="message-body">
-                        This website is a work in progress. Things might change radically.
+                        This website is currently being review before final launch. If you have any notes that you want
+                        to share with me, please DM me if you can or create an issue on the <a
+                        href="https://github.com/megoth/rdfjs/issues">website's project page</a>.
                     </div>
                 </div>
                 <Outlet/>
