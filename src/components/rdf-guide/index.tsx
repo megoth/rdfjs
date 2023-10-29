62ab0bd891664ea40836659d852fccfc3b824b5d
Arne Hassel <arne.hassel@capgemini.com>
2023-10-29 20:31:30 +0100

    Written more on the RDF guide

--- src/components/rdf-guide/index.tsx
+++ src/components/rdf-guide/index.tsx
@@ -1,5 +1,8 @@
 import Intro from "./1-intro.mdx";
-import BeyondRDF from "./2-beyond-rdf.mdx";
+import Vocabularies from "./2-vocabularies.mdx";
+import Shapes from "./3-shapes.mdx";
+import Querying from "./4-querying.mdx";
+import BeyondRDF from "./5-beyond-rdf.mdx";
 import {RDF_GUIDE} from "../../constants.ts";
 import GuideLayout from "../guide-layout";
 
@@ -7,6 +10,9 @@ export default function RDFGuide() {
     return (
         <GuideLayout guide={RDF_GUIDE}>
             <Intro/>
+            <Vocabularies />
+            <Shapes />
+            <Querying />
             <BeyondRDF/>
         </GuideLayout>
     )
