2094a3393e6779f614f7500591430b9604b9ffb5
Arne Hassel <arne.hassel@capgemini.com>
2023-11-02 00:22:45 +0100

    Trying again

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import server from "../dist/server";
-
-console.log("RUNNING SERVER", server);
-
-export default function handler(request, response) {
-    return response.send("Hello RDF + JS = ❤");
-}
