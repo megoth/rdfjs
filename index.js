07503135dbf9d1cdd0d1bc0693775515b28efd5c
Arne Hassel <arne.hassel@capgemini.com>
2023-11-02 00:53:32 +0100

    Something worked

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import "./dist/server/index.js";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.status(200).send("One step at a time");
+})
+
 // eslint-disable-next-line no-undef
 app.listen(process.env.PORT, () => {
     console.log("RUNNING SERVER");
