8059cd8f982683d4124fc764c08ac8e4d4480006
Arne Hassel <arne.hassel@capgemini.com>
2023-11-02 00:18:04 +0100

    Think its smart to add handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
 import server from "../dist/server";
 
 console.log("RUNNING SERVER", server);
+
+export default function handler(request, response) {
+    return response.send("Hello RDF + JS = ❤");
+}
