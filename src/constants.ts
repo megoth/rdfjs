4b06ac5a8003893b9a3b0202c04a4e2cea3e65e2
Arne Hassel <arne.hassel@capgemini.com>
2023-11-03 16:05:46 +0100

    Made default turtle easier to read on smaller screens

--- src/constants.ts
+++ src/constants.ts
@@ -478,8 +478,11 @@ export const PERSON_JSON = {
     name: "Soukai Test"
 };
 
-export const PROFILE_TURTLE = `<${PROFILE_URI}> a <http://xmlns.com/foaf/0.1/Person>;
-    <http://xmlns.com/foaf/0.1/name> "Test".
+export const PROFILE_TURTLE = `@prefix ex: <https://example.com/> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+
+ex:profile a foaf:Person;
+    foaf:name "Test".
 `;
 
 interface Provider {
