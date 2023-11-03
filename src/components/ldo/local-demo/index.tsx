631bb6986c48414029d2d585b1f15a333a278104
Arne Hassel <arne.hassel@capgemini.com>
2023-11-03 20:50:41 +0100

    Fixed a bug in local LDO demo

--- src/components/ldo/local-demo/index.tsx
+++ src/components/ldo/local-demo/index.tsx
@@ -6,11 +6,13 @@ import {PROFILE_TURTLE, PROFILE_URI, STORAGE_KEYS} from "../../../constants.ts";
 import {parseRdf, toTurtle} from "ldo";
 import Demo, {FormData} from "../../demo";
 import ErrorMessage from "../../error-message";
+import {useLdo} from "@ldo/solid-react";
 
 export default function LDOLocalDemo() {
     const [profile, setProfile] = useState<SolidProfile | null>(null);
     const [turtle, setTurtle] = useLocalStorage(STORAGE_KEYS.PROFILE_LDO, PROFILE_TURTLE);
     const [error, setError] = useState<Error | null>(null);
+    const {createData} = useLdo();
 
     useEffect(() => {
         parseRdf(turtle, {baseIRI: PROFILE_URI})
@@ -18,17 +20,18 @@ export default function LDOLocalDemo() {
             .catch((error) => setError(error));
     }, [turtle]);
 
-    if (!profile || !error) {
+    if (!profile && !error) {
         return <Loading/>
     }
 
     const onSubmit = async (data: FormData) => {
-        profile.name = data.name;
-        const turtle = await toTurtle(profile).catch(setError);
+        const oldProfile = profile || createData(SolidProfileShapeType, PROFILE_URI);
+        oldProfile.name = data.name;
+        const turtle = await toTurtle(oldProfile).catch(setError);
         if (turtle) setTurtle(turtle);
     };
 
     return error
         ? <ErrorMessage error={error}/>
-        : <Demo name={profile.name || ""} onSubmit={onSubmit}/>
+        : <Demo name={profile?.name || ""} onSubmit={onSubmit}/>
 }
