09d8414bfce286c9fdc2d17c1a54c8b1aea1146b
Arne Hassel <arne.hassel@capgemini.com>
2023-11-04 18:17:53 +0100

    Added install to TypeScript guide

--- src/components/typescript-guide/index.tsx
+++ src/components/typescript-guide/index.tsx
@@ -1,14 +1,16 @@
 import GuideLayout from "../guide-layout";
 import {TYPESCRIPT_GUIDE} from "../../constants.ts";
 import Intro from "./1-intro.mdx";
-import Types from "./2-types.mdx";
-import Interfaces from "./3-interfaces.mdx";
-import Outro from "./4-outro.mdx";
+import Install from "./2-install.mdx";
+import Types from "./3-types.mdx";
+import Interfaces from "./4-interfaces.mdx";
+import Outro from "./5-outro.mdx";
 
 export default function TypeScriptGuide() {
     return (
         <GuideLayout guide={TYPESCRIPT_GUIDE}>
             <Intro/>
+            <Install />
             <Types/>
             <Interfaces/>
             <Outro />
