690909f5a00c28d65848fc2ed781278620a040e2
Arne Hassel <arne.hassel@capgemini.com>
2023-11-04 05:13:58 +0100

    Improved readability of Soukai page

--- src/components/soukai/Person.ts
+++ src/components/soukai/Person.ts
@@ -1,4 +1,14 @@
-import Model from './Person.schema';
+import {FieldType} from 'soukai';
+import {defineSolidModelSchema} from 'soukai-solid';
+
+const Model = defineSolidModelSchema({
+    rdfContext: 'http://xmlns.com/foaf/0.1/',
+    rdfsClass: 'Person',
+    timestamps: false,
+    fields: {
+        name: FieldType.String
+    },
+});
 
 export default class Person extends Model {
 
