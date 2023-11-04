486510b0cfd3f884e76320411a462f341344c04f
Arne Hassel <arne.hassel@capgemini.com>
2023-11-04 02:51:57 +0100

    Reordered provider list

--- src/constants.ts
+++ src/constants.ts
@@ -504,10 +504,10 @@ interface Provider {
 
 export const PROVIDERS: Array<Provider> = [
     {
-        label: "Inrupt Pod Spaces",
-        logoSrc: "/logos/inrupt-ps-logo.png",
-        loginIri: "https://broker.pod.inrupt.com/",
-        signupIri: "https://signup.pod.inrupt.com/",
+        label: "solidcommunity.net",
+        logoSrc: "/logos/solid-emblem.svg",
+        loginIri: "https://solidcommunity.net/",
+        signupIri: "https://solidcommunity.net/register",
     },
     {
         label: "solidweb.me",
@@ -516,16 +516,16 @@ export const PROVIDERS: Array<Provider> = [
         signupIri: "https://solidweb.me/idp/register/",
     },
     {
-        label: "solidcommunity.net",
-        logoSrc: "/logos/solid-emblem.svg",
-        loginIri: "https://solidcommunity.net/",
-        signupIri: "https://solidcommunity.net/register",
-    },
-    {
         label: "inrupt.net",
         logoSrc: "/logos/solid-emblem.svg",
         loginIri: "https://inrupt.net/",
         signupIri: "https://inrupt.net/register",
+    },
+    {
+        label: "Inrupt Pod Spaces",
+        logoSrc: "/logos/inrupt-ps-logo.png",
+        loginIri: "https://broker.pod.inrupt.com/",
+        signupIri: "https://signup.pod.inrupt.com/",
     },
 ];
 
