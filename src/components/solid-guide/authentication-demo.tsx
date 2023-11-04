2e58381663b130bb9f297a0b5ba561e926011739
Arne Hassel <arne.hassel@capgemini.com>
2023-11-04 03:23:21 +0100

    Improved the auth demo in Solid guide

--- src/components/solid-guide/authentication-demo.tsx
+++ src/components/solid-guide/authentication-demo.tsx
@@ -1,20 +1,25 @@
 import {useSolidAuth} from "@ldo/solid-react";
 import Login from "../login";
+import {HTMLAttributes} from "react";
 
-export default function AuthenticationDemo() {
+interface Props extends HTMLAttributes<HTMLDivElement> {
+}
+
+export default function AuthenticationDemo({id, ...props}: Props) {
     const {login, logout, session: {isLoggedIn, webId}} = useSolidAuth();
 
     return isLoggedIn
-        ? <>
-            <div className="notification is-success is-light">
-                You're authenticated with <a href={webId}>{webId}</a>
+        ? <div id={id} {...props}>
+            <div className="notification is-success is-light" style={{overflow: "auto"}}>
+                <span>You're authenticated with </span>
+                <a href={webId}>{webId}</a>
             </div>
             <button className="button" onClick={logout}>Log out</button>
-        </>
-        : <>
+        </div>
+        : <div id={id} {...props}>
             <div className="notification is-danger is-light">
                 You're not authenticated
             </div>
-            <Login login={login}/>
-        </>
+            <Login login={login} redirectId={id}/>
+        </div>
 }
