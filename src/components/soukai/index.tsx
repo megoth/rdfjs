b4675434bd51e27f250eca123c01d82b288618b8
Arne Hassel <arne.hassel@capgemini.com>
2023-11-04 17:51:11 +0100

    Added install to Soukai

--- src/components/soukai/index.tsx
+++ src/components/soukai/index.tsx
@@ -1,8 +1,9 @@
 import Intro from "./1-intro.mdx";
-import Models from "./2-models.mdx";
-import LocalDemo from "./3-local-demo.mdx";
-import SolidDemo from "./4-solid-demo.mdx";
-import Review from "./5-review.mdx";
+import Install from "./2-install.mdx";
+import Models from "./3-models.mdx";
+import LocalDemo from "./4-local-demo.mdx";
+import SolidDemo from "./5-solid-demo.mdx";
+import Review from "./6-review.mdx";
 import {LIBRARY_SOUKAI} from "../../constants.ts";
 import LibraryLayout from "../library-layout";
 
@@ -10,6 +11,7 @@ export default function Soukai() {
     return (
         <LibraryLayout library={LIBRARY_SOUKAI}>
             <Intro/>
+            <Install />
             <Models />
             <LocalDemo />
             <SolidDemo />
