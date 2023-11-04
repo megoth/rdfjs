422e058003ec665af273e00c5944c167f5b2f2b1
Arne Hassel <arne.hassel@capgemini.com>
2023-11-04 18:10:28 +0100

    Added install sections to demo on frontpage and React guide

--- src/components/react-guide/index.tsx
+++ src/components/react-guide/index.tsx
@@ -1,15 +1,17 @@
 import GuideLayout from "../guide-layout";
 import {REACT_GUIDE} from "../../constants.ts";
 import Intro from "./1-intro.mdx";
-import JSX from "./2-jsx.mdx";
-import StateHandling from "./3-state-handling.mdx";
-import Community from "./4-community.mdx";
-import Alternatives from "./5-alternatives.mdx";
+import Install from "./2-install.mdx";
+import JSX from "./3-jsx.mdx";
+import StateHandling from "./4-state-handling.mdx";
+import Community from "./5-community.mdx";
+import Alternatives from "./6-alternatives.mdx";
 
 export default function ReactGuide() {
     return (
         <GuideLayout guide={REACT_GUIDE}>
             <Intro />
+            <Install />
             <JSX />
             <StateHandling />
             <Community />
