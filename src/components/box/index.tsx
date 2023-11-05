85b52c4157eaea1aa7253b6df9fbfda92cf218fb
Arne Hassel <arne.hassel@capgemini.com>
2023-11-05 06:24:26 +0100

    Reverting small padding on smaller screens
    Looks weird

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,7 +1,6 @@
 import {HTMLAttributes} from "react";
 import {clsx} from "clsx";
-import styles from "./style.module.css";
 
 export default function Box({children, className, ...props}: HTMLAttributes<HTMLDivElement>) {
-    return <section className={clsx("box", styles.box, className)} {...props}>{children}</section>
+    return <section className={clsx("box", className)} {...props}>{children}</section>
 }
