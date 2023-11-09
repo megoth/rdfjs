8497403497734562680812f5f460caa577414914
Arne Hassel <arne.hassel@capgemini.com>
2023-11-09 15:15:44 +0100

    Bootstrapped business guide

--- src/components/business-guide/index.tsx
+++ src/components/business-guide/index.tsx
@@ -2,14 +2,12 @@ import GuideLayout from "../guide-layout";
 import {BUSINESS_GUIDE} from "../../constants";
 import Intro from "./1-intro.mdx";
 import UseCases from "./2-use-cases.mdx";
-import Outro from "./3-outro.mdx";
 
 export default function BusinessGuide() {
     return (
         <GuideLayout guide={BUSINESS_GUIDE}>
-            <Intro/>
-            <UseCases/>
-            <Outro/>
+            <Intro />
+            <UseCases />
         </GuideLayout>
     )
 }
