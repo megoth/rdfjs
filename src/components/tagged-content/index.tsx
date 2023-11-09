e56c2d85bb9afc33a0371d0e9a99e0ca00bfc4c1
Arne Hassel <arne.hassel@capgemini.com>
2023-11-09 21:05:26 +0100

    Improved design a bit

--- src/components/tagged-content/index.tsx
+++ src/components/tagged-content/index.tsx
@@ -2,6 +2,8 @@ import Content from "../content";
 import {HTMLAttributes} from "react";
 import {TagModel} from "../../constants.tsx";
 import Tag from "../tag";
+import {clsx} from "clsx";
+import styles from "./styles.module.css";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
     tags: Array<TagModel>
@@ -12,7 +14,7 @@ export default function TaggedContent({children, tags, title, ...props}: Props) 
         <Content {...props}>
             {title && <h3 className="subtitle">{title}</h3>}
 
-            {tags.length > 0 && <div className={"tags"}>
+            {tags.length > 0 && <div className={clsx("tags", styles.tags)}>
                 {tags.map((tag) => <Tag key={tag.name} tag={tag}/>)}
             </div>}
 
