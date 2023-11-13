c5a3baab727c5799a83702036b0c8854a6451344
Arne Hassel <arne.hassel@capgemini.com>
2023-11-13 19:05:41 +0100

    Need slightly different config for dev and build
    Need to verify that this works on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,32 +5,58 @@ import {NodeGlobalsPolyfillPlugin} from "@esbuild-plugins/node-globals-polyfill"
 import {NodeModulesPolyfillPlugin} from '@esbuild-plugins/node-modules-polyfill'
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const baseConfig = {
     plugins: [
         {enforce: 'pre', ...mdx()},
         react(),
     ],
-    optimizeDeps: {
-        esbuildOptions: {
-            // Node.js global to browser globalThis
-            define: {
-                global: 'globalThis'
-            },
-            // Enable esbuild polyfill plugins
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true
-                }),
-                NodeModulesPolyfillPlugin(),
-            ]
-        }
-    },
     build: {
         rollupOptions: {
             plugins: [
                 rollupNodePolyFill()
             ]
+        },
+    }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(({command}) => {
+    if (command === "serve") { //dev config
+        return {
+            ...baseConfig,
+            optimizeDeps: {
+                esbuildOptions: {
+                    // Node.js global to browser globalThis
+                    define: {
+                        global: 'globalThis'
+                    },
+                    // Enable esbuild polyfill plugins
+                    plugins: [
+                        NodeGlobalsPolyfillPlugin({
+                            buffer: true
+                        }),
+                    ]
+                }
+            },
         }
     }
+    return { // prod config
+        ...baseConfig,
+        optimizeDeps: {
+            esbuildOptions: {
+                // Node.js global to browser globalThis
+                define: {
+                    global: 'globalThis'
+                },
+                // Enable esbuild polyfill plugins
+                plugins: [
+                    NodeGlobalsPolyfillPlugin({
+                        buffer: true
+                    }),
+                    NodeModulesPolyfillPlugin(),
+                ]
+            }
+        },
+    }
+    return {}
 })
