ce6047b3de9d1d8958a1f1c87f7811fdf191b7a1
Arne Hassel <arne.hassel@capgemini.com>
2023-11-13 01:44:03 +0100

    Fixing some bugs so that Vercel can build

--- src/components/m-ld/solid-demo/index.tsx
+++ src/components/m-ld/solid-demo/index.tsx
@@ -1,38 +1,5 @@
-import {useEffect, useMemo, useState} from "react";
-import {useSolidAuth} from "@ldo/solid-react";
-import Demo, {FormData} from "../../demo";
-import Loading from "../../loading";
-import {clone, uuid} from '@m-ld/m-ld';
-import {MemoryLevel} from 'memory-level';
-import {IoRemotes} from "@m-ld/m-ld/ext/socket.io";
+import Demo from "../../demo";
 
 export default function MLdSolidDemo() {
-    // const {session: {webId}, fetch} = useSolidAuth();
-    // const [error, setError] = useState<Error | null>(null);
-    // const domainUrl = useMemo(() => `${uuid()}.public.gw.m-ld.org`, []);
-    //
-    // useEffect(() => {
-    //     const config = {
-    //         '@id': uuid(),
-    //         '@domain': domainUrl,
-    //         genesis: true,
-    //         io: {uri: "https://gw.m-ld.org"}
-    //     };
-    //     clone(new MemoryLevel(), IoRemotes, config).then((meld) => {
-    //         console.log("CLONE", meld);
-    //     })
-    // }, [domainUrl, fetch]);
-    //
-    // if (!error) {
-    //     return <Loading/>
-    // }
-    //
-    // const onSubmit = async (data: FormData) => {
-    //     setError(null);
-    //     // update
-    //     console.log(webId, data);
-    // };
-    //
-    // return <Demo error={error} name={"Not set"} onSubmit={onSubmit}/>
     return <Demo name={"Not set"} onSubmit={console.log} />
 }
