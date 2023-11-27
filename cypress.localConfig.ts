52bf263b39f9ec1508b0f1193fc641ad3a4e1b4f
Arne Hassel <arne.hassel@capgemini.com>
2023-11-27 15:41:10 +0100

    Making sure we have slightly diff configs for local and GH Action

--- cypress.localConfig.ts
+++ cypress.localConfig.ts
@@ -3,7 +3,7 @@ import customViteConfig from "./vite.config";
 
 export default defineConfig({
     e2e: {
-        baseUrl: "http://localhost:3000"
+        baseUrl: "http://localhost:5173"
     },
     component: {
         devServer: {
