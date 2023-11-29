aa401e8ce10f6cec2222f3fc51e43de86c97a9f7
Arne Hassel <arne.hassel@capgemini.com>
2023-11-29 22:41:07 +0100

    Added section on interoperability

--- src/components/rdf-guide/index.tsx
+++ src/components/rdf-guide/index.tsx
@@ -1,9 +1,10 @@
 import Intro from "./1-intro.mdx";
-import Serializations from "./2-serializations.mdx";
-import Vocabularies from "./3-vocabularies.mdx";
-import Shapes from "./4-shapes.mdx";
-import Querying from "./5-querying.mdx";
-import BeyondRDF from "./6-beyond-rdf.mdx";
+import Interoperability from "./2-interoperability.mdx";
+import Serializations from "./3-serializations.mdx";
+import Vocabularies from "./4-vocabularies.mdx";
+import Shapes from "./5-shapes.mdx";
+import Querying from "./6-querying.mdx";
+import BeyondRDF from "./7-beyond-rdf.mdx";
 import {RDF_GUIDE} from "../../constants";
 import GuideLayout from "../guide-layout";
 
@@ -11,6 +12,7 @@ export default function RDFGuide() {
     return (
         <GuideLayout guide={RDF_GUIDE}>
             <Intro/>
+            <Interoperability />
             <Serializations />
             <Vocabularies />
             <Shapes />
