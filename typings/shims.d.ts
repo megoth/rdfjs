bbc1c10f17403f7014006fc657a5a4ea0694a244
Arne Hassel <arne.hassel@gmail.com>
2025-02-14 16:18:06 +0100

    An attempt at the Solid demo
    Able to read name, but not update it

--- typings/shims.d.ts
+++ typings/shims.d.ts
@@ -4,14 +4,9 @@ declare module '*.mdx' {
     export default ReactComponent
 }
 
-declare module "rdf-sparql-builder" {
-    export const deleteQuery: function;
-    export const select: function;
-}
-
-declare module 'solid-namespace' {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    export default (factory: unknown) => Record<string, (label: string) => string>;
+declare module '@solid/query-ldflex' {
+    export default any;
+    export const PathFactory: function;
 }
 
 declare module '@tpluscode/sparql-builder' {
@@ -21,4 +16,19 @@ declare module '@tpluscode/sparql-builder' {
 
 declare module '@zazuko/rdf-vocabularies' {
     export const prefixes: Record<string, string>
+}
+
+declare module 'ldflex' {
+    export default any;
+    export const PathFactory: function;
+}
+
+declare module "rdf-sparql-builder" {
+    export const deleteQuery: function;
+    export const select: function;
+}
+
+declare module 'solid-namespace' {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    export default (factory: unknown) => Record<string, (label: string) => string>;
 }
